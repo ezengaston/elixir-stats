3c097b4f2d16658f1cfb858c716902347ee681ca
Ezen <ezen.gaston@gmail.com>
2023-07-11 01:41:10 +0300

    Added population mean

---
+++ lib/central_tendency/mean.ex
@@ -0,0 +1,27 @@
+defmodule Stats.CentralTendency.Mean do
+  alias Stats.{Errors, Validators}
+
+  def population_mean([]), do: Errors.invalid_data_type()
+
+  def population_mean(nums) when is_list(nums) do
+    nums
+    |> Validators.validate_num_list()
+    |> calc_population_mean()
+  end
+
+  def population_mean(_), do: Errors.invalid_data_type()
+
+  # ================================================================
+
+  def calc_population_mean({false, _nums}), do: Errors.invalid_data_type()
+
+  def calc_population_mean({true, nums}) do
+    nums
+    |> Enum.sum()
+    |> mean(Enum.count(nums))
+  end
+
+  # ================================================================
+
+  def mean(sigma, count), do: sigma / count
+end
